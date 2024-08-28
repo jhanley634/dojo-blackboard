f63da440fc18e6a007caa5a49ba2e3dd600b091e
John Hanley <jhanley708-git@s6.sector6.net>
2024-08-27 23:15:36 -0700

    Add table_of_contents

--- src/bboard/main.py
+++ src/bboard/main.py
@@ -10,24 +10,19 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 from src.bboard.demo.greeting import greeting
+from src.bboard.util.web import table_of_contents
 
 app = FastAPI()
 
 
-@app.get("/", response_class=HTMLResponse)
-async def root() -> HTMLResponse:
-    """Table of contents, so folks may easily click on various endpoints."""
-    html = """
-    <h1>Dojo Blackboard</h1>
-    <ul>
-      <li><a href="hello">/hello</a></li>
-    </ul>
-    """
-    return HTMLResponse(content=html)
-
-
-@app.get("/hello")
+@app.get("/demo/hello")
 async def hello() -> dict[str, str]:
     # We keep main.py small, delegating most endpoint logic in separate modules.
     # Please follow this pattern when adding new endpoints.
     return dict(greeting())
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root() -> HTMLResponse:
+    """A ToC that allows folks to easily click on various endpoints."""
+    return HTMLResponse(content=table_of_contents(app.routes))
