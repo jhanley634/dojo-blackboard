b94720a5ee84ffd4535257f81c364c22660d5e6f
John Hanley <jhanley708-git@s6.sector6.net>
2024-08-28 23:16:54 -0700

    Reduce the minimum vehicle count

--- tests/vehicles_test.py
+++ tests/vehicles_test.py
@@ -6,4 +6,4 @@ from src.bboard.transit.vehicles import query_vehicles
 class VehiclesTest(unittest.TestCase):
     def test_query_vehicles(self) -> None:
         v = query_vehicles()
-        self.assertGreater(len(v), 100)  # typically 200-300
+        self.assertGreater(len(v), 20)  # typically 200-300
