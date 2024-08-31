87163f28555d80a162d82acd41117ff0810e6ba9
John Hanley <jhanley708-git@s6.sector6.net>
2024-08-30 18:47:58 -0700

    Use @contextmanager to ensure we commit() at the end

--- src/bboard/database.py
+++ src/bboard/database.py
@@ -1,8 +1,16 @@
+from collections.abc import Generator
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
 engine = create_engine(url="sqlite:////tmp/blackboard.db")
 
 
-def get_session() -> Session:
-    return sessionmaker(bind=engine)()
+@contextmanager
+def get_session() -> Generator[Session, None, None]:
+    sess = sessionmaker(bind=engine)()
+    try:
+        yield sess
+    finally:
+        sess.commit()
--- src/bboard/transit/iss.py
+++ src/bboard/transit/iss.py
@@ -13,6 +13,10 @@ ISS_URL = "http://api.open-notify.org/iss-now.json"
 
 
 def iss_lng_lat() -> tuple[float, float]:
+    """Returns the current longitude and latitude of the ISS.
+
+    Side effect: Writes current position to the DB.
+    """
     resp = get(ISS_URL)
     resp.raise_for_status()
     j = resp.json()
@@ -23,6 +27,5 @@ def iss_lng_lat() -> tuple[float, float]:
 
     with get_session() as sess:
         sess.add(IssPosition(stamp=stamp, longitude=lng, latitude=lat))
-        sess.commit()
 
     return lng, lat
