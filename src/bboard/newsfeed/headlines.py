d59dd726f176aab2698ab89efa2e4207f1461c15
John Hanley <jhanley708-git@s6.sector6.net>
2024-09-30 10:58:29 -0700

    Add KNOWN_GOOD_SOURCES

--- src/bboard/newsfeed/headlines.py
+++ src/bboard/newsfeed/headlines.py
@@ -2,12 +2,14 @@ import datetime as dt
 import json
 from base64 import urlsafe_b64encode
 from hashlib import sha3_224
+from pprint import pp
 from typing import Any
 from urllib.parse import urlparse
 
 from requests import get  # type: ignore [attr-defined]
 
 from bboard.models.article import Article
+from bboard.newsfeed.sources import FILTERED_SOURCES, KNOWN_GOOD_SOURCES
 from bboard.util.database import get_session
 
 BASE_URL = "https://ok.surf/api/v1/news-feed"
@@ -27,15 +29,25 @@ def _get_article_hashes() -> set[str]:
 CATEGORIES_OF_INTEREST = ["Business", "Health", "Science", "Technology", "US", "World"]
 
 
+def _href(url: str) -> str:
+    return f'<a href="{url}">{url}</a>'
+
+
 def store_current_articles() -> dict[str, Any]:
     hashes = _get_article_hashes()
     resp = get(BASE_URL)
     resp.raise_for_status()
     d: dict[str, Any] = json.loads(resp.text)
+    known = KNOWN_GOOD_SOURCES | FILTERED_SOURCES
 
     with get_session() as sess:
         for category in CATEGORIES_OF_INTEREST:
             for article in d[category]:
+                if article["source"] not in known:
+                    src = article["source"]
+                    print(f'\n<p>"{src}",<p>\n')
+                    print(_href(article["link"]), "\n")
+
                 if article["source"][0].islower():
                     article["source"] = article["source"].title()
                 article["stamp"] = dt.datetime.now(dt.UTC)
---
+++ src/bboard/newsfeed/sources.py
@@ -0,0 +1,54 @@
+"""
+This module contains a set of servers that are filtered out from the newsfeed
+due to low quality content or requiring a subscription.
+"""
+
+FILTERED_SOURCES = {
+    "Bloomberg",
+    "Forbes",
+    "Haaretz",
+    "The New York Times",
+    "The Wall Street Journal",
+    "The Washington Post",
+}
+
+KNOWN_GOOD_SOURCES = {
+    "Al Jazeera English",
+    "BBC.com",
+    "Benzinga",
+    "Bitcoinist",
+    "CBS News",
+    "CNBC",
+    "CNN",
+    "Daily Kos",
+    "Daily Mail",
+    "Engadget",
+    "Fortune",
+    "Fox Business",
+    "Fox News",
+    "Gizmodo",
+    "HuffPost",
+    "Investor's Business Daily",
+    "KHON2",
+    "Kyiv Independent",
+    "Live and Let's Fly",
+    "NPR",
+    "New York Post",
+    "Newsweek",
+    "POLITICO Europe",
+    "POLITICO",
+    "Reuters",
+    "Slate",
+    "South China Morning Post",
+    "TESLARATI",
+    "The Arizona Republic",
+    "The Associated Press",
+    "The Daily Beast",
+    "The Motley Fool",
+    "The Times of Israel",
+    "The Verge",
+    "TipRanks",
+    "USA TODAY",
+    "Yahoo Finance",
+    "Yahoo Life",
+}
