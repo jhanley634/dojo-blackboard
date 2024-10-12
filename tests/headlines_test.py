73fa25c96c6a4b20b99296a05731b3f33eb2ff55
John Hanley <jhanley708-git@s6.sector6.net>
2024-10-11 21:31:02 -0700

    Add test_sources

--- tests/headlines_test.py
+++ tests/headlines_test.py
@@ -2,9 +2,14 @@ import unittest
 from warnings import catch_warnings, simplefilter
 
 from bboard.newsfeed.headlines import _href, store_current_articles
+from bboard.newsfeed.sources import FILTERED_SOURCES, KNOWN_GOOD_SOURCES
 
 
 class HeadlinesTest(unittest.TestCase):
+
+    def test_sources(self) -> None:
+        self.assertEqual(7, len(FILTERED_SOURCES))
+        self.assertEqual(86, len(KNOWN_GOOD_SOURCES))
 
     def test_href(self) -> None:
         self.assertEqual(
