558966f0aa54bb64c51d15ea51fcbc82977266a2
John Hanley <jhanley708-git@s6.sector6.net>
2024-10-28 11:54:08 -0700

    Add Counts.__str__

--- src/count/sloc.py
+++ src/count/sloc.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 from pathlib import Path
+from typing import NamedTuple
 
 import typer
 
@@ -10,27 +11,33 @@ def get_source_files(folder: Path) -> list[Path]:
     return [file for file in files if file.is_file()]
 
 
-class LineCounter:
+class Counts(NamedTuple):
+    blanks: int
+    comments: int
+    code: int
 
+    def __str__(self) -> str:
+        return f"{self.blanks:5d} blanks   {self.comments:5d} comments   {self.code:5d} code"
+
+
+class LineCounter:
     def __init__(self, in_file: Path) -> None:
         with open(in_file) as fin:
             self.lines = list(map(str.rstrip, fin))
-        self.blanks = sum(1 for line in self.lines if not line)
-        self.comments = sum(1 for line in self.lines if line.startswith("#"))
-        self.code = len(self.lines) - self.blanks - self.comments
 
-    def counts(self) -> dict[str, int]:
-        return {
-            "blanks": self.blanks,
-            "comments": self.comments,
-            "code": self.code,
-        }
+    def counts(self) -> Counts:
+        c = Counts(
+            blanks=sum(1 for line in self.lines if not line),
+            comments=sum(1 for line in self.lines if line.startswith("#")),
+            code=0,
+        )
+        return c._replace(code=len(self.lines) - c.blanks - c.comments)
 
 
 def main(in_folder: Path) -> None:
     for file in get_source_files(in_folder):
         cnt = LineCounter(file)
-        print(f"{len(cnt.lines):9,d}  lines in  {file}")
+        print(f"{cnt.counts()}  lines in  {file}")
 
 
 if __name__ == "__main__":
