4e1a2f3be86aace25116437fb11142e271c57048
John Hanley <jhanley708-git@s6.sector6.net>
2024-10-29 16:13:07 -0700

    Add _get_non_blank_lines()

--- src/count/sloc.py
+++ src/count/sloc.py
@@ -1,9 +1,16 @@
 #! /usr/bin/env python
 import re
 from collections import Counter
+from collections.abc import Generator
+from enum import Enum, auto
 from pathlib import Path
 
 import typer
+
+
+class LineType(Enum):
+    COMMENT = auto()
+    CODE = auto()
 
 
 def get_source_files(folder: Path) -> list[Path]:
@@ -21,13 +28,30 @@ class LineCounter:
             lines = list(map(str.rstrip, fin))
 
         self.blank = sum(1 for line in lines if not line)
-        self.comment = len(list(filter(_strip_hash_re.search, lines)))
-        self.code = sum(1 for line in lines if _strip_hash_re.sub("", line).strip())
+        line_types = list(self._get_line_types(lines))
+        self.comment = sum(1 for line_type in line_types if line_type == LineType.COMMENT)
+        self.code = sum(1 for line_type in line_types if line_type == LineType.CODE)
 
         assert self.blank + self.comment + self.code == len(lines), len(lines)
 
     def __str__(self) -> str:
         return f"{self.blank:5d} blank   {self.comment:5d} comment   {self.code:5d} code"
+
+    def _get_line_types(self, lines: list[str]) -> Generator[LineType, None, None]:
+        for line in self._get_non_blank_lines(lines):
+            if _strip_hash_re.match(line):
+                yield LineType.COMMENT
+            else:
+                yield LineType.CODE
+
+    def _get_non_blank_lines(self, lines: list[str]) -> Generator[str, None, None]:
+        """This is `grep -v '^$'`.
+
+        Recall that trailing whitespace has already been stripped.
+        """
+        for line in lines:
+            if line:
+                yield line
 
 
 def main(in_folder: Path) -> None:
