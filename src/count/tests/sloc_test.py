d428d2cd60f8fb5e093fc20cc69134da3c0fc8d7
John Hanley <jhanley708-git@s6.sector6.net>
2024-11-11 12:00:57 -0800

    Use portable /dev/null

--- src/count/tests/sloc_test.py
+++ src/count/tests/sloc_test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from contextlib import redirect_stdout
 from pathlib import Path
@@ -11,7 +12,8 @@ SOURCES = [
     _REPOS / "docker-php-tutorial",
 ]
 
-INITIAL_SOURCES = sorted(get_source_files(SOURCES[0]))[:3]
+INITIAL_CPP_SOURCES = sorted(get_source_files(SOURCES[0]))[:3]
+INITIAL_PHP_SOURCES = sorted(get_source_files(SOURCES[1]))[:3]
 
 
 class SlocTest(unittest.TestCase):
@@ -21,7 +23,7 @@ class SlocTest(unittest.TestCase):
         with redirect_stdout(None):
             main(SOURCES[0])
 
-    def test_count_lines(self) -> None:
+    def test_count_cpp_lines(self) -> None:
         for folder in SOURCES:
             assert folder.is_dir(), "Please run: $ make count"
 
@@ -31,10 +33,10 @@ class SlocTest(unittest.TestCase):
                 "/tmp/repos/llama.cpp/common/common.cpp",
                 "/tmp/repos/llama.cpp/common/console.cpp",
             ],
-            list(map(str, INITIAL_SOURCES)),
+            list(map(str, INITIAL_CPP_SOURCES)),
         )
 
-        cnt = LineCounter(INITIAL_SOURCES[0])
+        cnt = LineCounter(INITIAL_CPP_SOURCES[0])
         self.assertEqual(
             {"blank": 47, "comment": 32, "code": 1973},
             cnt.__dict__,
@@ -46,8 +48,31 @@ class SlocTest(unittest.TestCase):
             cnt.__dict__,
         )
 
+    def test_count_php_lines(self) -> None:
+        cnt = LineCounter(INITIAL_PHP_SOURCES[0])
+        self.assertEqual(
+            {"blank": 11, "comment": 9, "code": 34},
+            cnt.__dict__,
+        )
+
+        self.assertEqual(
+            _REPOS / "docker-php-tutorial/app/Console/Kernel.php",
+            INITIAL_PHP_SOURCES[1],
+        )
+        cnt = LineCounter(INITIAL_PHP_SOURCES[1])
+        self.assertEqual(
+            {"blank": 5, "comment": 12, "code": 15},
+            cnt.__dict__,
+        )
+
+        cnt = LineCounter(_REPOS / "docker-php-tutorial/config/database.php")
+        self.assertEqual(
+            {"blank": 23, "comment": 45, "code": 107},
+            cnt.__dict__,
+        )
+
     def test_expand_comments_multiline(self) -> None:
-        cnt = LineCounter(Path("/dev/null"))
+        cnt = LineCounter(Path(os.devnull))
         lines = [
             "zero /* comment */ calories",
             "/* one",
@@ -69,8 +94,15 @@ class SlocTest(unittest.TestCase):
         )
 
     def test_expand_comments_single_line(self) -> None:
-        cnt = LineCounter(Path("/dev/null"))
-        lines = [" /**/ foo", "bar", " /* qux */ baz /*", "blorg */", "a /* b */ c /* d */ e"]
+        cnt = LineCounter(Path(os.devnull))
+        lines = [
+            " /**/ foo",
+            "bar",
+            " /* qux */ baz /*",
+            "blorg */",
+            "a /* b */ c /* d */ e",
+            " /* b */ c /* d */ e",
+        ]
         self.assertEqual(
             [
                 "   foo",
@@ -78,6 +110,7 @@ class SlocTest(unittest.TestCase):
                 "   baz /*",
                 "// ",
                 "a   e",
+                "   e",
             ],
             list(cnt.expand_comments(lines)),
         )
