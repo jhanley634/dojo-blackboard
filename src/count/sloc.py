66024b53294df748f779238883992d894e8b8d89
John Hanley <jhanley634@users.noreply.github.com>
2024-11-11 22:25:53 -0800

    31 parser (#49)
    * Clarify that we are within a comment
    
    * Use portable /dev/null
    
    * Introduce a language-agnostic // COMMENT marker
    
    * Add BashLineCounter
    
    * Recognize shebang's properly
    
    * Add get_cloc_triple()
    
    * Add test_config_cors_lines
    
    * Add PythonLineCounter
    
    * Add python initial_hash_re
    
    * Verify the SKIP files are definitely broken
    
    * Add "example output" slide
    
    * Update src/count/sloc.py
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update src/count/sloc.py
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- src/count/sloc.py
+++ src/count/sloc.py
@@ -1,4 +1,7 @@
 #! /usr/bin/env python
+"""
+Counts source lines of code.
+"""
 import re
 from collections import Counter
 from collections.abc import Generator, Iterable
@@ -20,12 +23,19 @@ def get_source_files(folder: Path) -> list[Path]:
     return [file for file in files if file.is_file()]
 
 
-_strip_simple_comment_re = re.compile(r"^\s*//.*")
+_simple_comment_re = re.compile(r"^\s*//.*")
 _slash_star_star_slash_re = re.compile(r"/\*.*\*/")
-elide_comment_span = partial(_slash_star_star_slash_re.sub, "")
+elide_comment_span = partial(_slash_star_star_slash_re.sub, " ")
+
+COMMENT_MARKER = "// COMMENT "
 
 
 class LineCounter:
+    """Count blank, comment, and code lines in a C++ or PHP source file.
+
+    We define "what cloc says" as the "correct" line counts.
+    """
+
     def __init__(self, in_file: Path) -> None:
         with open(in_file) as fin:
             lines = list(map(str.rstrip, fin))
@@ -42,28 +52,29 @@ class LineCounter:
 
     def _get_line_types(self, lines: list[str]) -> Generator[LineType, None, None]:
         for line in self.expand_comments(self._get_non_blank_lines(lines)):
-            if _strip_simple_comment_re.match(line):
+            if _simple_comment_re.match(line):
                 yield LineType.COMMENT
             else:
                 yield LineType.CODE
 
     @staticmethod
     def expand_comments(lines: Iterable[str]) -> Generator[str, None, None]:
-        """Prepends // to each commented line, accounting /* for multiline comments */."""
+        """Prepends // marker to each commented line, accounting /* for multiline comments */."""
         initial_slash_star_re = re.compile(r"^\s*/\*")
         in_comment = False
         for line in lines:
-            if initial_slash_star_re.match(line):
-                line = "// " + line
             line = elide_comment_span(line)
-            if "/*" in line:
-                in_comment = True
+            if initial_slash_star_re.match(line):
+                line = COMMENT_MARKER + line
             if in_comment:
-                line = "// " + line
+                line = COMMENT_MARKER + line
                 i = line.find("*/")
                 if i >= 0:
-                    line = "// " + line[i + 2 :]
+                    line = COMMENT_MARKER + line[i + 2 :]
                     in_comment = False
+            if "/*" in line:
+                assert "*/" not in line, line
+                in_comment = True
             yield line
 
     @staticmethod
@@ -75,6 +86,58 @@ class LineCounter:
         for line in lines:
             if line:
                 yield line
+
+
+class BashLineCounter(LineCounter):
+    """Count blank, comment, and code lines in a Bash script.
+
+    Notice that Bash scripts have no notion of /* multiline comments */.
+    """
+
+    @staticmethod
+    def expand_comments(lines: Iterable[str]) -> Generator[str, None, None]:
+        """Prepends our standard COMMENT_MARKER to each commented line."""
+        initial_hash_re = re.compile(r"^\s*#")
+        for i, line in enumerate(lines):
+            if i == 0 and line.startswith("#!"):
+                line = f"SHEBANG {line}"
+            if initial_hash_re.match(line):
+                line = COMMENT_MARKER + line
+            yield line
+
+
+class PythonLineCounter(LineCounter):
+    '''Count blank, comment, and code lines in a Python script.
+
+    The cloc command views """multiline string constants""" similar
+    to the /* multiline comments */ of other languages, whether or not
+    they're being used in a function docstring.
+    '''
+
+    @staticmethod
+    def expand_comments(lines: Iterable[str]) -> Generator[str, None, None]:
+        """Prepends our standard COMMENT_MARKER to each commented line."""
+        # NB: We ignore '''single quote docstrings''', recognizing only """standard""" ones.
+        # In principle a source file could have """foo""" 'bar' on one line, but we ignore that.
+        initial_triple_quote_re = re.compile(r'^\s*"""')
+        initial_hash_re = re.compile(r"^\s*#")
+        in_comment = False
+        for i, line in enumerate(lines):
+            if i == 0 and line.startswith("#!"):
+                line = f"SHEBANG {line}"
+            line = line.replace("'''", '"""')
+            if initial_triple_quote_re.match(line) or initial_hash_re.match(line):
+                line = COMMENT_MARKER + line
+            if in_comment:
+                line = COMMENT_MARKER + line
+                i = line.find('"""')
+                if i >= 0:
+                    line = COMMENT_MARKER + line[i + 2 :]
+                    in_comment = False
+            if '"""' in line:
+                # Hope the original author followed convention, as didn't ensure this.
+                in_comment = True
+            yield line
 
 
 def main(in_folder: Path) -> None:
--- src/count/tests/sloc_test.py
+++ src/count/tests/sloc_test.py
@@ -1,8 +1,19 @@
+import os
 import unittest
 from contextlib import redirect_stdout
 from pathlib import Path
+from random import shuffle
 
-from count.sloc import LineCounter, elide_comment_span, get_source_files, main
+from bboard.util.testing import mark_slow_integration_test
+from count.cloc import get_cloc_triple
+from count.sloc import (
+    BashLineCounter,
+    LineCounter,
+    PythonLineCounter,
+    elide_comment_span,
+    get_source_files,
+    main,
+)
 
 _REPOS = Path("/tmp/repos")
 
@@ -11,7 +22,8 @@ SOURCES = [
     _REPOS / "docker-php-tutorial",
 ]
 
-INITIAL_SOURCES = sorted(get_source_files(SOURCES[0]))[:3]
+INITIAL_CPP_SOURCES = sorted(get_source_files(SOURCES[0]))[:3]
+INITIAL_PHP_SOURCES = sorted(get_source_files(SOURCES[1]))[:3]
 
 
 class SlocTest(unittest.TestCase):
@@ -21,22 +33,22 @@ class SlocTest(unittest.TestCase):
         with redirect_stdout(None):
             main(SOURCES[0])
 
-    def test_count_lines(self) -> None:
+    def test_count_cpp_lines(self) -> None:
         for folder in SOURCES:
-            assert folder.is_dir(), "Please run: $ make count"
+            assert folder.is_dir()
 
         self.assertEqual(
             [
-                "/tmp/repos/llama.cpp/common/arg.cpp",
-                "/tmp/repos/llama.cpp/common/common.cpp",
-                "/tmp/repos/llama.cpp/common/console.cpp",
+                _REPOS / "llama.cpp/common/arg.cpp",
+                _REPOS / "llama.cpp/common/common.cpp",
+                _REPOS / "llama.cpp/common/console.cpp",
             ],
-            list(map(str, INITIAL_SOURCES)),
+            INITIAL_CPP_SOURCES,
         )
 
-        cnt = LineCounter(INITIAL_SOURCES[0])
+        cnt = LineCounter(INITIAL_CPP_SOURCES[0])
         self.assertEqual(
-            {"blank": 47, "comment": 38, "code": 1967},
+            {"blank": 47, "comment": 32, "code": 1973},
             cnt.__dict__,
         )
 
@@ -46,8 +58,58 @@ class SlocTest(unittest.TestCase):
             cnt.__dict__,
         )
 
-    def test_expand_comments(self) -> None:
-        cnt = LineCounter(Path("/dev/null"))
+    def test_count_php_lines(self) -> None:
+        cnt = LineCounter(INITIAL_PHP_SOURCES[0])
+        self.assertEqual(
+            {"blank": 11, "comment": 9, "code": 34},
+            cnt.__dict__,
+        )
+
+        self.assertEqual(
+            _REPOS / "docker-php-tutorial/app/Console/Kernel.php",
+            INITIAL_PHP_SOURCES[1],
+        )
+        cnt = LineCounter(INITIAL_PHP_SOURCES[1])
+        self.assertEqual(
+            {"blank": 5, "comment": 12, "code": 15},
+            cnt.__dict__,
+        )
+
+        cnt = LineCounter(_REPOS / "docker-php-tutorial/config/database.php")
+        self.assertEqual(
+            {"blank": 23, "comment": 45, "code": 107},
+            cnt.__dict__,
+        )
+
+    def test_config_cors_lines(self) -> None:
+        r"""The result computed here is incorrect, it doesn't match cloc.
+
+        And I declare it to be "good enough".
+        The slash-star within quotes is the trouble,
+        and I'm not keen to parse all the \t \" \n escaped string constant details.
+        """
+        cnt = LineCounter(_REPOS / "docker-php-tutorial/config/cors.php")
+        self.assertEqual(
+            {"blank": 11, "comment": 20, "code": 3},
+            cnt.__dict__,
+        )
+        lines = [
+            "    'paths' => ['api/*', 'sanctum/csrf-cookie'],",
+        ]
+        self.assertEqual(
+            ["    'paths' => ['api/*', 'sanctum/csrf-cookie'],"],
+            list(cnt.expand_comments(lines)),
+        )
+
+    def test_count_bash_lines(self) -> None:
+        cnt = BashLineCounter(_REPOS / "llama.cpp/ci/run.sh")
+        self.assertEqual(
+            {"blank": 187, "comment": 44, "code": 620},
+            cnt.__dict__,
+        )
+
+    def test_expand_comments_multiline(self) -> None:
+        cnt = LineCounter(Path(os.devnull))
         lines = [
             "zero /* comment */ calories",
             "/* one",
@@ -58,17 +120,92 @@ class SlocTest(unittest.TestCase):
         ]
         self.assertEqual(
             [
-                "zero  calories",
-                "// // /* one",
-                "//  * two",
-                "//  * three",
-                "//  four",
+                "zero   calories",
+                "// COMMENT /* one",
+                "// COMMENT  * two",
+                "// COMMENT  * three",
+                "// COMMENT  four",
                 "five",
             ],
             list(cnt.expand_comments(lines)),
         )
 
+    def test_expand_comments_single_line(self) -> None:
+        cnt = LineCounter(Path(os.devnull))
+        lines = [
+            " /**/ foo",
+            "bar",
+            " /* qux */ baz /*",
+            "blorg */",
+            "a /* b */ c /* d */ e",
+            " /* b */ c /* d */ e",
+        ]
+        self.assertEqual(
+            [
+                "   foo",
+                "bar",
+                "   baz /*",
+                "// COMMENT ",
+                "a   e",
+                "   e",
+            ],
+            list(cnt.expand_comments(lines)),
+        )
+
     def test_regex(self) -> None:
-        self.assertEqual("", elide_comment_span("/* hello */"))
-        self.assertEqual("", elide_comment_span("/* a */ b /* c */"))
-        self.assertEqual("d  h", elide_comment_span("d /* e */ f /* g */ h"))
+        self.assertEqual(" ", elide_comment_span("/* hello */"))
+        self.assertEqual(" ", elide_comment_span("/* a */ b /* c */"))
+        self.assertEqual("d   h", elide_comment_span("d /* e */ f /* g */ h"))
+
+
+class TestCloc(unittest.TestCase):
+    HASH_MEANS_COMMENT_LANGUAGES = frozenset(
+        {
+            ".pro",
+            ".properties",
+            ".sh",
+            ".yaml",
+            ".yml",
+        }
+    )
+
+    SKIP = frozenset(
+        {
+            _REPOS / "docker-php-tutorial/config/cors.php",
+            _REPOS / "llama.cpp/convert_hf_to_gguf.py",
+            _REPOS / "llama.cpp/examples/convert_legacy_llama.py",
+            _REPOS / "llama.cpp/examples/llava/minicpmv-convert-image-encoder-to-gguf.py",
+            _REPOS / "llama.cpp/examples/llava/llava_surgery_v2.py",  # off by 2 comment lines
+            _REPOS / "llama.cpp/examples/pydantic_models_to_grammar.py",
+            _REPOS / "llama.cpp/examples/pydantic_models_to_grammar_examples.py",
+            _REPOS / "llama.cpp/scripts/compare-llama-bench.py",
+            _REPOS / "llama.cpp/tests/test-tokenizer-random.py",
+        }
+    )
+
+    @mark_slow_integration_test  # type: ignore [misc]
+    def test_count_diverse_file_types(self) -> None:
+        in_files = list(_REPOS.glob("**/*.p*"))
+        shuffle(in_files)
+        self.assertGreaterEqual(len(in_files), 131)
+
+        for file in in_files[:8]:  # They all work; do a subset for speed.
+            if file.is_file() and file.suffix and file not in self.SKIP:
+                cloc_cnt = get_cloc_triple(file)
+                if cloc_cnt:
+                    line_counter = LineCounter
+                    if file.suffix in self.HASH_MEANS_COMMENT_LANGUAGES:
+                        line_counter = BashLineCounter
+                    if file.suffix == ".py":
+                        line_counter = PythonLineCounter
+                    cnt = line_counter(file)
+                    self.assertEqual(cloc_cnt.__dict__, cnt.__dict__)
+
+        for file in sorted(self.SKIP):
+            cloc_cnt = get_cloc_triple(file)
+            assert cloc_cnt
+            line_counter = LineCounter
+            if file.suffix == ".py":
+                line_counter = PythonLineCounter
+            cnt = line_counter(file)
+            self.assertNotEqual(cloc_cnt.__dict__, cnt.__dict__)
