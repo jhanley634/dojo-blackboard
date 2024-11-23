4c2859734715576f7799276ab3c573a7c6b63c0d
John Hanley <jhanley708-git@s6.sector6.net>
2024-11-23 11:13:12 -0800

    Introduce d1 & d2

--- src/count/bisect.py
+++ src/count/bisect.py
@@ -66,7 +66,9 @@ class DiscrepancyFinder:
     @lru_cache
     def _counts_equal(self, n: int) -> bool:
         cloc_cnt, cnt = self._get_both_counts(n)
-        return dict(cloc_cnt.__dict__) == dict(cnt.__dict__)
+        d1: dict[str, int] = cloc_cnt.__dict__
+        d2: dict[str, int] = cnt.__dict__
+        return d1 == d2
 
     def _get_both_counts(self, n: int) -> tuple[ClocCounts, LineCounter]:
         temp_file = TEMP / f"upto_{n}{self.suffix}"
