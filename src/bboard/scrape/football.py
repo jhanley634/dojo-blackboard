02866ada0c65d19804ead5eed3954eb5d3a3b032
John Hanley <jhanley634@users.noreply.github.com>
2025-05-06 12:10:16 -0700

    Produce a MarkDown result table

--- src/bboard/scrape/football.py
+++ src/bboard/scrape/football.py
@@ -1,27 +1,32 @@
 #! /usr/bin/env python
 
 from datetime import timedelta
+from io import StringIO
 
 import pandas as pd
 import requests
 import requests_cache
-from bs4 import BeautifulSoup
 
 _one_day = timedelta(days=1).total_seconds()
 requests_cache.install_cache("/tmp/scraping_cache", expire_after=_one_day)
 
+
 target_url = (
-    "https://www.cnbc.com/2025/05/05/" "cnbcs-official-global-soccer-team-valuations-2025.html"
+    "https://www.cnbc.com/2025/05/05/cnbcs-official-global-soccer-team-valuations-2025.html"
 )
 ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:137.0) Gecko/20100101 Firefox/137.0"
 
 
 def main() -> None:
     resp = requests.get(target_url, headers={"User-Agent": ua})
-    print(resp.status_code)
-    html = resp.content
-    soup = BeautifulSoup(html, "html.parser")
-    print(soup.prettify())
+    resp.raise_for_status()
+
+    dfs = pd.read_html(StringIO(resp.text))
+    assert 1 == len(dfs)
+    df = dfs[0]
+    print(df.to_markdown(index=False))
+
+    # print(BeautifulSoup(html, "html.parser").prettify())
 
 
 if __name__ == "__main__":
