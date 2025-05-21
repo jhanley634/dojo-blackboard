6ec34a69e96bd866cf3af8469fe4cdf786dd7862
John Hanley <jhanley634@users.noreply.github.com>
2025-05-20 17:01:01 -0700

    Delete src/beeware-tutorial, as there was little interest in it. The toga package offers poor support for python 3.13

--- src/beeware-tutorial/helloworld/src/helloworld/__main__.py
+++
@@ -1,4 +0,0 @@
-from helloworld.app import main  # type: ignore [attr-defined]
-
-if __name__ == "__main__":
-    main().main_loop()
--- src/beeware-tutorial/helloworld/tests/app_test.py
+++
@@ -1,3 +0,0 @@
-def test_first() -> None:
-    """An initial test for the app."""
-    assert 1 + 1 == 2
