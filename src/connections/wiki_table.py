7c9b20604cd4ce32d6bb7805aa9eb8d921b1bdea
John Hanley <jhanley634@users.noreply.github.com>
2025-07-09 16:40:01 -0700

    Add wiki_table

---
+++ src/connections/wiki_table.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+from pathlib import Path
+
+import pandas as pd
+
+TEMP = Path("/tmp")
+
+
+def get_pres(out_file: Path = TEMP / "pres.csv") -> None:
+    """Reads several HTML tables from the given wiki page."""
+    url = "https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States"
+    df = pd.read_html(url)[0]
+    # print(df.to_markdown())
+    print(df.to_json())
+
+
+if __name__ == "__main__":
+    get_pres()
