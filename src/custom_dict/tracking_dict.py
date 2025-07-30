d44ffe2091a66689e63f7a3627c9576da887204b
John Hanley <jhanley634@users.noreply.github.com>
2025-07-30 16:52:18 -0700

    72 tracking dict (#73)
    * Initial step:  write a failing test
    
    * Inherit from UserDict
    
    * Take care to exercise d.items() -- it just works, no edits needed in the target code
    
    * Take care to exercise d.update() -- again, it just works
    
    * Add test_union()
    
    * Add test_popitem()
    
    * Add test_copy(). Had to change the target code, due to a problematic test of  if self.__class__ is UserDict:
    
    * Add test_deepcopy() and __deepcopy__()
    
    * Adopt sourcery-ai's suggestion to type hint as Any

---
+++ src/custom_dict/tracking_dict.py
@@ -0,0 +1,46 @@
+from collections import UserDict
+from collections.abc import Generator
+from copy import deepcopy
+from typing import Any
+
+
+class TrackingDict(UserDict[Any, Any]):
+    """
+    A dict that tracks whether the app ever bothered to read what it stored.
+
+    This can help with finding things like overly verbose DB queries,
+    application reporting function bugs, and API mismatches.
+    Call the .unread_keys() generator to find entries that have not yet been read.
+    """
+
+    def __init__(
+        self, initial_data: dict[Any, Any] | None = None, /, **kwargs: dict[Any, Any]
+    ) -> None:
+        d: dict[Any, Any] = initial_data or {}
+        super().__init__(d, **kwargs)
+        self.used: set[Any] = set()  # Keys the app has read / consumed
+
+    def copy(self) -> "TrackingDict":
+        c = TrackingDict()
+        c.update(self)
+        return c
+
+    def __deepcopy__(self, _memo: dict[Any, Any]) -> "TrackingDict":
+        c = TrackingDict()
+        c.used = deepcopy(self.used)
+        for k, v in self.items():
+            c[k] = deepcopy(v)
+        return c
+
+    def __delitem__(self, key: Any) -> None:
+        self.used.discard(key)
+        return super().__delitem__(key)
+
+    def __getitem__(self, key: Any) -> Any:
+        self.used.add(key)
+        return super().__getitem__(key)
+
+    def unread_keys(self) -> Generator[Any]:
+        for k in self.keys():
+            if k not in self.used:
+                yield k
---
+++ src/custom_dict/tracking_dict_test.py
@@ -0,0 +1,70 @@
+import unittest
+from copy import deepcopy
+
+from custom_dict.tracking_dict import TrackingDict
+
+
+class TrackingDictTest(unittest.TestCase):
+    def test_tracking_dict(self) -> None:
+        d = TrackingDict()
+        d["a"] = 1
+        d["c"] = 3
+        d["b"] = 2
+        self.assertEqual([1, 3, 2], list(d.values()))
+        self.assertEqual([], list(d.unread_keys()))
+
+        d["d"] = 4
+        d["e"] = 6
+        d["e"] = 5
+        self.assertEqual(["d", "e"], list(d.unread_keys()))
+        del d["d"]
+        self.assertEqual(5, d["e"])
+        self.assertEqual([], list(d.unread_keys()))
+
+    def test_items(self) -> None:
+        d = TrackingDict({"a": 1, "b": 2, "c": 3, "d": 4})
+        self.assertEqual(3, d["c"])
+        self.assertEqual("abd", "".join(d.unread_keys()))
+
+        for i, (_k, _v) in enumerate(d.items()):
+            if i >= 1:
+                break
+        self.assertEqual(["d"], list(d.unread_keys()))
+
+    def test_update(self) -> None:
+        d = TrackingDict({"a": 1, "b": 2, "c": 3, "d": 4})
+        d.update({"b": 12, "x": 13, "y": 14, "z": 15})
+        del d["c"]
+        del d["y"]
+        self.assertEqual("abdxz", "".join(d.unread_keys()))
+
+    def test_union(self) -> None:
+        """Same as test_update(), pretty much."""
+        d1 = TrackingDict({"a": 1, "b": 2, "c": 3, "d": 4})
+        d = d1 | {"b": 12, "x": 13, "y": 14, "z": 15}
+        del d["c"]
+        del d["y"]
+        self.assertEqual("abdxz", "".join(d.unread_keys()))
+
+    def test_popitem(self) -> None:
+        d = TrackingDict({"a": 1, "b": 2, "c": 3, "d": 4})
+        self.assertEqual(("a", 1), d.popitem())
+        self.assertEqual(("b", 2), d.popitem())
+        self.assertEqual("cd", "".join(d.unread_keys()))
+
+        self.assertEqual(3, d.pop("c"))
+        self.assertEqual("d", "".join(d.unread_keys()))
+
+    def test_copy(self) -> None:
+        d1 = TrackingDict({"a": 1, "b": 2, "c": 3, "d": 4})
+        d = d1.copy()
+        self.assertEqual("", "".join(d1.unread_keys()))
+        self.assertEqual("abcd", "".join(d.unread_keys()))
+
+    def test_deepcopy(self) -> None:
+        d1 = TrackingDict({"a": 1, "b": 2, "c": 3, "d": 4})
+        d = deepcopy(d1)
+        self.assertEqual("", "".join(d1.unread_keys()))
+
+        self.assertEqual(3, d["c"])
+        self.assertEqual("abd", "".join(d.unread_keys()))
