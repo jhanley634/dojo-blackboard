d3e1fc6617cf5c56221b0c9e82fb11f317cb7563
John Hanley <jhanley634@users.noreply.github.com>
2025-07-30 17:45:56 -0700

    74 pickle dict (#75)
    * Verify the used keys got serialized
    
    * Check equality on the attributes

--- src/custom_dict/tracking_dict_test.py
+++ src/custom_dict/tracking_dict_test.py
@@ -1,5 +1,7 @@
+import pickle
 import unittest
 from copy import deepcopy
+from pathlib import Path
 
 from custom_dict.tracking_dict import TrackingDict
 
@@ -68,3 +70,19 @@ class TrackingDictTest(unittest.TestCase):
 
         self.assertEqual(3, d["c"])
         self.assertEqual("abd", "".join(d.unread_keys()))
+
+    def test_pickle_roundtrip(self) -> None:
+        d = TrackingDict({"a": 1, "b": 2, "c": 3, "d": 4})
+        self.assertEqual(3, d["c"])
+        pkl = Path("/tmp/dict.pkl")
+        with open(pkl, "wb") as f:
+            pickle.dump(d, f)
+
+        with open(pkl, "rb") as f:
+            d1 = pickle.load(f)
+
+        self.assertEqual(list(d.unread_keys()), list(d1.unread_keys()))
+        self.assertEqual(d.used, d1.used)
+        self.assertEqual(d.data, d1.data)
+        self.assertEqual(d, d1)
+        pkl.unlink()
