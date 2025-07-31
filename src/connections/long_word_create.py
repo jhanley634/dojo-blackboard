27503c5bb0f127a535ef9fd08748ae78a7eb1891
John Hanley <jhanley634@users.noreply.github.com>
2025-07-31 09:54:01 -0700

    Add table_exists()

--- src/connections/long_word_create.py
+++ src/connections/long_word_create.py
@@ -9,9 +9,15 @@ the queries issued by find_longest_match().
 
 from pathlib import Path
 
-from sqlalchemy import Column, Engine, String, __version__, create_engine
+from sqlalchemy import Column, Engine, String, __version__, create_engine, inspect
 from sqlalchemy.orm import Session, declarative_base
 from sqlalchemy.orm.decl_api import DeclarativeMeta
+
+
+def table_exists(table_name: str) -> bool:
+    inspector = inspect(get_engine())
+    return inspector.has_table(table_name)
+
 
 TEMP = Path("/tmp")
 
@@ -37,6 +43,7 @@ ENGLISH_WORDS = Path("/usr/share/dict/words")
 def etl(in_file: Path = ENGLISH_WORDS) -> None:
     engine = get_engine()
     Base.metadata.create_all(engine)
+    assert table_exists("word")
     seen = set()
     with Session(engine) as sess, open(in_file) as fin:
         # Clear out all rows before we start inserting, to avoid dup PK.
