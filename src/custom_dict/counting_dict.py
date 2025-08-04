5657a362cc80bf0b61fe1922e20430c292a28a0b
John Hanley <jhanley634@users.noreply.github.com>
2025-08-04 15:59:47 -0700

    Add self.count to the access counter ctor

--- src/custom_dict/counting_dict.py
+++ src/custom_dict/counting_dict.py
@@ -1,6 +1,14 @@
-from collections import UserDict
+from collections import Counter, UserDict
 from typing import Any
 
 
 class AccessCounterDict(UserDict[Any, Any]):
-    pass
+    # > Write a class AccessCounterDict that extends dict to count accesses to keys.
+    # NB: This class extends a different class, rather than dict.
+
+    def __init__(
+        self, initial_data: dict[Any, Any] | None = None, /, **kwargs: dict[Any, Any]
+    ) -> None:
+        d: dict[Any, Any] = initial_data or {}
+        super().__init__(d, **kwargs)
+        self.count: Counter[Any] = Counter()  # per-key access counts
--- src/custom_dict/counting_dict_test.py
+++ src/custom_dict/counting_dict_test.py
@@ -6,8 +6,10 @@ from custom_dict.tracking_dict_test import _example_mapping
 
 class AccessCounterDictTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.d = AccessCounterDict(_example_mapping())
+        self.d = AccessCounterDict(dict(_example_mapping()))
 
     def test_access_counter_dict(self) -> None:
         d = self.d.copy()
         self.assertEqual(4, len(d))
+        self.assertEqual(5, d["b"] + d["c"])
+        self.assertEqual(0, len(d.count))
