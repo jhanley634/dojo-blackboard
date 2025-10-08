f1add88e41d545166620b11c9fb77dd470dc1dfd
John Hanley <jhanley634@users.noreply.github.com>
2025-10-07 20:55:04 -0700

    Add find_two_sum()

--- src/challenge/two_sum/find_two_sum.py
+++ src/challenge/two_sum/find_two_sum.py
@@ -1,6 +1,9 @@
+from sortedcontainers import SortedList
+
+
 def find_two_sum_quadratic(arr: list[int], target: int) -> tuple[int, int]:
     """
-    Finds the two numbers that sum to a target number.
+    Finds the two numbers x + y that sum to a target number.
 
     We are guaranteed a unique solution;
     exactly two of the inputs will sum to the desired target.
@@ -10,7 +13,25 @@ def find_two_sum_quadratic(arr: list[int], target: int) -> tuple[int, int]:
     for x in arr:
         for y in arr:
             if x + y == target:
-                return (x, y)
+                return x, y
+
+    diagnostic = f"The input arr must contain a pair which sums to {target}"
+    raise ValueError(diagnostic)
+
+
+def find_two_sum(arr: list[int], target: int) -> tuple[int, int]:
+    """
+    Finds the two numbers x + y that sum to a target number.
+
+    We are guaranteed a unique solution;
+    exactly two of the inputs will sum to the desired target.
+
+    A linear O(n) pass lets us complete the task in O(n log n) time.
+    """
+    xs = SortedList(arr)
+    for y in (target - x for x in xs):
+        if y in xs:
+            return target - y, y
 
     diagnostic = f"The input arr must contain a pair which sums to {target}"
     raise ValueError(diagnostic)
--- src/challenge/two_sum/find_two_sum_test.py
+++ src/challenge/two_sum/find_two_sum_test.py
@@ -1,14 +1,21 @@
 import unittest
 
-from challenge.two_sum.find_two_sum import find_two_sum_quadratic
+from challenge.two_sum.find_two_sum import find_two_sum, find_two_sum_quadratic
 
 arr = [7, 3, 5, 9]
 target = 8
 
 
 class FindTwoSumTest(unittest.TestCase):
+
     def test_quadratic(self) -> None:
         self.assertEqual(
             (3, 5),
             find_two_sum_quadratic(arr, target),
+        )
+
+    def test_find_two_sum(self) -> None:
+        self.assertEqual(
+            (3, 5),
+            find_two_sum(arr, target),
         )
