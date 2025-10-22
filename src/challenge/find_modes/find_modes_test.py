7d26c16dee66712a692478572905ddb06120a9eb
John Hanley <jhanley634@users.noreply.github.com>
2025-10-22 11:13:34 -0700

    Prefer to return an NDArray

--- src/challenge/find_modes/find_modes_test.py
+++ src/challenge/find_modes/find_modes_test.py
@@ -3,23 +3,27 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+from numpy.typing import NDArray
 
 
-def fetch_input_data(n: int) -> list[int]:
+def fetch_input_data(n: int) -> NDArray[np.int32]:
     data_dir = Path("src/challenge/find_modes/data")
     assert data_dir.exists()
 
-    csv = data_dir / f"{n}_random_numbers.txt"
+    csv = data_dir / f"{n:,}_random_numbers.txt".replace(",", "_")
     if csv.exists():
         df = pd.read_csv(csv, header=None, names=["n"])
-        return df.n.to_list()
+        return np.array(df.n.astype(np.int32).to_numpy())
 
-    rng = np.random.default_rng()
-    return list(rng.integers(0, 1000, size=1_000_000, dtype=np.int32))
+    rng = np.random.default_rng(seed=42)
+    return rng.integers(0, 1_000, size=1_000_000, dtype=np.int32)
 
 
 class FindModesTest(unittest.TestCase):
 
     def test_fetch(self) -> None:
-        xs = fetch_input_data(1001)
-        print(xs)
+        xs = fetch_input_data(10_000)
+        xs = fetch_input_data(100)
+        xs = fetch_input_data(200)
+        for x in xs:
+            assert 0 <= x < 1_000
