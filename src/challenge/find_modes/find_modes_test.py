698e9f78bba64178528d73d2e982a95162facc03
John Hanley <jhanley634@users.noreply.github.com>
2025-10-22 12:39:28 -0700

    Add get_runs()

--- src/challenge/find_modes/find_modes_test.py
+++ src/challenge/find_modes/find_modes_test.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 from numpy.typing import NDArray
 
+from challenge.find_modes.sorting import find_modes_via_sorting, get_runs
+
 
 def fetch_input_data(n: int) -> NDArray[np.int32]:
     data_dir = Path("src/challenge/find_modes/data")
@@ -27,3 +29,46 @@ class FindModesTest(unittest.TestCase):
         xs = fetch_input_data(200)
         for x in xs:
             assert 0 <= x < 1_000
+
+    def test_get_runs(self) -> None:
+        tests = [
+            ([(1, 7)], [7]),
+            ([(2, 7)], [7, 7]),
+            ([(3, 7)], [7, 7, 7]),
+            ([(1, 7), (1, 9)], [7, 9]),
+            ([(1, 7), (2, 9)], [7, 9, 9]),
+            ([(1, 7), (3, 8), (2, 9)], [7, 8, 8, 8, 9, 9]),
+        ]
+        for expected, xs in tests:
+            self.assertEqual(
+                expected,
+                list(get_runs(np.array(xs))),
+            )
+
+        xs = [2, 3, 4, 99, 5, 6, 7]
+        with self.assertRaises(ValueError):
+            list(get_runs(np.array(xs)))
+
+    def test_sorting(self) -> None:
+        tests = [
+            (100, [188, 208, 374, 546, 641, 694]),
+            (200, [242]),
+            (10_000, [284]),
+            (1_000_000, [242]),
+        ]
+        self.assertEqual(
+            [188, 208, 374, 546, 641, 694],
+            find_modes_via_sorting(fetch_input_data(100)),
+        )
+        self.assertEqual(
+            [242],
+            find_modes_via_sorting(fetch_input_data(200)),
+        )
+        self.assertEqual(
+            [284],
+            find_modes_via_sorting(fetch_input_data(10_000)),
+        )
+        self.assertEqual(
+            [242],
+            find_modes_via_sorting(fetch_input_data(1_000_000)),
+        )
---
+++ src/challenge/find_modes/sorting.py
@@ -0,0 +1,37 @@
+from collections.abc import Generator
+
+import numpy as np
+from numpy.typing import NDArray
+
+
+def find_modes_via_sorting(xs: NDArray[np.int32]) -> list[int]:
+    xs.sort()
+
+    max_n = 0
+    for n, _ in get_runs(xs):
+        max_n = max(n, max_n)
+
+    ret: list[int] = []
+    for n, value in get_runs(xs):
+        if max_n == n:
+            ret.append(value)
+
+    return ret
+
+
+def get_runs(xs: NDArray[np.int32]) -> Generator[tuple[int, int]]:
+    """Generates a (count, value) tuple for each run of an observed value."""
+    n = 0
+    prev = xs[0]  # input shall be non-empty
+
+    for x in xs:
+        if x < prev:
+            raise ValueError  # input must be monotonic increasing
+        if x == prev:
+            n += 1
+        else:
+            yield n, int(prev)
+            n = 1
+            prev = x
+
+    yield n, int(prev)
